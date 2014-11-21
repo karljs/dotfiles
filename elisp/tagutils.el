54b57a6e957b954fd54ea6c1249a7d07c559a13c
Karl Smeltzer <karl.smeltzer@gmail.com>
2014-11-21 00:01:27 -0800

    experiment with proper package

--- elisp/tagutils.el
+++ elisp/tagutils.el
@@ -1,19 +1,44 @@
-(defun htags (dir)
-  "Create Haskell tags"
+;;; tagutils.el --- Automatically generate appropriate tag files
+
+;; Author: Karl Smeltzer <karl.smeltzer@gmail.com>
+;; Package-Version: 0.1
+;; URL: http://github.com/karljs/tagutils
+
+;;; Commentary:
+
+;; This package provides some simple helper utilities for automatically
+;; generating tag files appropriate for the language being used.  This is
+;; useful for languages that aren't covered by Exuberant Ctags, such as
+;; Haskell.
+;;
+;; This is very primitive and not really intended for wider use, as it does not
+;; support many languages, nor mixed-language projects, or anything else fancy.
+
+;;; Code:
+
+;;;###autoload
+(defun tagutils-haskell-tags (dir)
+  "Generate Haskell tags using Hasktags"
   (interactive)
   (shell-command (concat "cd " dir "&& hasktags -e -o ./TAGS ."))
   (message "Created Haskell tags."))
 
-(defun ctags (dir)
+;;;###autoload
+(defun tagutils-ctags-tags (dir)
   "Create ctags"
   (interactive)
   (shell-command (concat "cd " dir "&& ctags -e -R ."))
   (message "Created ctags."))
 
-(defun tags (dir)
-  "Create tags"
+;;;###autoload
+(defun tagutils-gen-tags (dir)
+  "Look at the current major mode and generate tags for the project"
   (interactive (list (read-directory-name "Directory: ")))
   (if (equal major-mode 'haskell-mode)
       (htags dir)
     (ctags dir)))
 ;; (global-set-key (kbd "C-c t") 'tags)
+
+(provide 'tagutils)
+
+;;; tagutils.el ends here
