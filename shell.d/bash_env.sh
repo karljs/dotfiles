fc714ce32e1bc9bdd2fd8f3c6dadccea09002574
Karl Smeltzer <git@karlsmeltzer.com>
2025-08-08 14:12:19 -0700

    zellij

--- shell.d/bash_env.sh
+++ shell.d/bash_env.sh
@@ -7,6 +7,10 @@ export VISUAL="hx"
 # disable XON/XOFF so C-s can forward search
 stty -ixon
 
+if command -v zellij >/dev/null 2>&1; then
+  eval "$(zellij setup --generate-completion bash)"
+fi
+
 # Haskell tooling
 [ -f "/home/karl/.ghcup/env" ] && . "/home/karl/.ghcup/env" # ghcup-env
 
@@ -16,6 +20,6 @@ PATH="/home/karl/.cargo/bin:$PATH"
 export PATH
 
 # prompt
-if command -v starship > /dev/null 2>&1; then
+if command -v starship >/dev/null 2>&1; then
   eval "$(starship init bash)"
 fi
