e3d9cbc63176c125be0a313c89cf6b0df1018ef5
Karl Smeltzer <karl.smeltzer@gmail.com>
2025-08-10 13:21:52 -0700

    set path before checking commands

--- shell.d/bash_env.sh
+++ shell.d/bash_env.sh
@@ -7,9 +7,8 @@ export VISUAL="hx"
 # disable XON/XOFF so C-s can forward search
 stty -ixon
 
-if command -v zellij >/dev/null 2>&1; then
-  eval "$(zellij setup --generate-completion bash)"
-fi
+# misc local stuff
+PATH="/home/karl/.local/bin:$PATH"
 
 # Haskell tooling
 [ -f "/home/karl/.ghcup/env" ] && . "/home/karl/.ghcup/env" # ghcup-env
@@ -18,6 +17,11 @@ fi
 PATH="/home/karl/.cargo/bin:$PATH"
 
 export PATH
+
+# must come after PATH is set
+if command -v zellij >/dev/null 2>&1; then
+  eval "$(zellij setup --generate-completion bash)"
+fi
 
 # prompt
 if command -v starship >/dev/null 2>&1; then
