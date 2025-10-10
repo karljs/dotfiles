b96f19728e412d0026e8a0ba31196537430264d0
Karl Smeltzer <karl.smeltzer@canonical.com>
2025-10-10 18:44:30 -0400

    just open a dir when switching projects

--- emacs.d/config/kjs-nav.el
+++ emacs.d/config/kjs-nav.el
@@ -140,6 +140,8 @@
   :ensure t
   :init
   (projectile-mode +1)
+  :config
+  (setq projectile-switch-project-action #'projectile-dired)
   :bind (:map projectile-mode-map
               ("s-p" . projectile-command-map)
               ("C-c p" . projectile-command-map)))
