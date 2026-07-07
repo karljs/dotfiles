6629cd3e8432429cc02167994a19fc8915d93cfe
Karl Smeltzer <karl.smeltzer@gmail.com>
2026-07-06 22:22:31 -0700

    auto treesitter mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -476,14 +476,12 @@
   (c++-mode . eglot-ensure)
   (c++-ts-mode . eglot-ensure)
   (rust-mode . eglot-ensure)
+  (rust-ts-mode . eglot-ensure)
   (python-mode . eglot-ensure)
+  (python-ts-mode . eglot-ensure)
   (haskell-mode . eglot-ensure)
   :config
   (setq eglot-autoshutdown t)
-  (setq major-mode-remap-alist
-        '((c-mode . c-ts-mode)
-          (c++-mode . c++-ts-mode)
-          (python-mode . python-ts-mode)))
 
   (add-hook 'eglot-managed-mode-hook
             (lambda ()
@@ -499,50 +497,11 @@
   :bind (("C-c C-s" . consult-eglot-symbols)))
 
 
-(use-package treesit
-  :preface
-  (defun kjs-ts-url (proj)
-    (concat "https://github.com/tree-sitter/tree-sitter-" proj))
-
-  (defun kjs-setup-install-grammars ()
-    "Install tree-sitter grammars if missing."
-    (interactive)
-    (dolist (grammar
-             `((c . (,(kjs-ts-url "c") "v0.23.5"))
-               (cmake . ("https://github.com/uyha/tree-sitter-cmake" "v0.7.2"))
-               (cpp . (,(kjs-ts-url "cpp") "v0.23.4"))
-               (css . (,(kjs-ts-url "css") "v0.20.0"))
-               (go . (,(kjs-ts-url "go") "v0.20.0"))
-               (html . (,(kjs-ts-url "html") "v0.20.1"))
-               (javascript . (,(kjs-ts-url "javascript") "v0.20.1" "src"))
-               (json . (,(kjs-ts-url "json") "v0.20.2"))
-               (markdown . ("https://github.com/ikatyang/tree-sitter-markdown" "v0.7.1"))
-               (python . (,(kjs-ts-url "python") "v0.20.4"))
-               (rust . (,(kjs-ts-url "rust") "v0.21.2"))
-               (toml . (,(kjs-ts-url "toml") "v0.5.1"))
-               (tsx . (,(kjs-ts-url "typescript") "v0.20.3" "tsx/src"))
-               (typescript . (,(kjs-ts-url "typescript") "v0.20.3" "typescript/src"))
-               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
-      (add-to-list 'treesit-language-source-alist grammar)
-      (unless (treesit-language-available-p (car grammar))
-        (treesit-install-language-grammar (car grammar)))))
-
-  (dolist (mapping
-           '((python-mode . python-ts-mode)
-             (css-mode . css-ts-mode)
-             (typescript-mode . typescript-ts-mode)
-             (js2-mode . js-ts-mode)
-             (bash-mode . bash-ts-mode)
-             (conf-toml-mode . toml-ts-mode)
-             (go-mode . go-ts-mode)
-             (css-mode . css-ts-mode)
-             (json-mode . json-ts-mode)
-             (js-json-mode . json-ts-mode)
-             (c-mode . c-ts-mode)
-             (c++-mode . c++-ts-mode)))
-    (add-to-list 'major-mode-remap-alist mapping))
+(use-package treesit-auto
+  :ensure t
   :config
-  (kjs-setup-install-grammars))
+  (setq treesit-auto-install 'prompt)
+  (global-treesit-auto-mode))
 
 ;;; Programming Languages
 
@@ -599,8 +558,6 @@
 (use-package rust-mode
   :after eglot
   :ensure
-  :init
-  (setq rust-mode-treesitter-derive t)
   :config
   (add-to-list 'eglot-server-programs
                '((rust-ts-mode rust-mode) .
